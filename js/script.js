c66d949502e5e6ee7f16ced8a19f2c4d98978ce7
Vladislav Mararenko <mararenko.vladislav@gmail.com>
2022-09-30 21:47:25 +0300

    first task fix

--- js/script.js
+++ js/script.js
@@ -1,17 +1,5 @@
-function whatTimeIsIt() {
-  const quantity = Number(prompt('Write amount of minutes'));
-  const hours = Math.floor(quantity / 60);
-  const minutes = quantity % 60;
-  const doubleHours = String(hours).padStart(2, 0);
-  const doubleMinutes = String(minutes).padStart(2, 0);
-
-  console.log(`${doubleHours}:${doubleMinutes}`);
-}
-
-whatTimeIsIt();
-
 function time() {
-  let quantity = Number(prompt('Ведіть кількість хвилин.'));
+  let quantity = Number(prompt("Ведіть кількість хвилин."));
   let hours = Math.floor(quantity / 60);
   let minutes = quantity % 60;
   const normHours = String(hours).padStart(2, 0);
